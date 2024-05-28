cc5340cf5367a3dc6c9feed4b92b878605ef768b
Lucas Freymund <lucasehresmann.osik@gmail.com>
2024-05-28 01:36:07 -0300

    Seller Form and Seller Database Update

--- WebSalesControl/Controllers/SellersController.cs
+++ WebSalesControl/Controllers/SellersController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebSalesControl.Models;
 using WebSalesControl.Services;
 
 namespace WebSalesControl.Controllers
@@ -16,6 +17,19 @@ namespace WebSalesControl.Controllers
         {
             var list = _sellerService.FindAll();
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller) 
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
--- WebSalesControl/Services/SellerService.cs
+++ WebSalesControl/Services/SellerService.cs
@@ -16,5 +16,12 @@ namespace WebSalesControl.Services
         {
             return _context.Seller.ToList();
         }
+
+        public void Insert(Seller seller)
+        {
+            seller.Department = _context.Department.First();
+            _context.Add(seller);
+            _context.SaveChanges();
+        }
     }
 }
