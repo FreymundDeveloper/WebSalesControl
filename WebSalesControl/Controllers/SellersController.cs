0440acdb5d5b2130e581b29c35e0041842c2e2e0
Lucas Freymund <lucasehresmann.osik@gmail.com>
2024-05-29 23:57:29 -0300

    Model Validations

--- WebSalesControl/Controllers/SellersController.cs
+++ WebSalesControl/Controllers/SellersController.cs
@@ -45,6 +45,13 @@ namespace WebSalesControl.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Seller seller) 
         {
+            if (!ModelState.IsValid)
+            {
+                var departments = _departmentService.FindAll();
+                var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+                return View(viewModel);
+            }
+
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
@@ -85,6 +92,13 @@ namespace WebSalesControl.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Edit(int id, Seller seller)
         {
+            if (!ModelState.IsValid)
+            {
+                var departments = _departmentService.FindAll();
+                var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+                return View(viewModel);
+            }
+
             if (id != seller.Id) return RedirectToAction(nameof(Error), new { message = "Id mismatch!" });
 
             try
